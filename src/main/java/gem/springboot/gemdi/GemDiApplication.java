01d33a81c5cdc73c4d7e4e403b25c1e19f94a748
dhanashree4051 <dhanashreec.gem@gmail.com>
2022-01-20 19:08:57 +0530

    To display fake data username and password.

--- src/main/java/gem/springboot/gemdi/GemDiApplication.java
+++ src/main/java/gem/springboot/gemdi/GemDiApplication.java
@@ -1,6 +1,7 @@
 package gem.springboot.gemdi;
 
 import gem.springboot.gemdi.controller.*;
+import gem.springboot.gemdi.datasource.FakeDataSource;
 import gem.springboot.gemdi.services.PrototypeBean;
 import gem.springboot.gemdi.services.SingletonBean;
 import org.springframework.boot.SpringApplication;
@@ -18,12 +19,16 @@ public class GemDiApplication {
 		ApplicationContext applicationContext = SpringApplication.run(GemDiApplication.class, args);
 		MyController myController = (MyController) applicationContext.getBean("myController");
 
+		System.out.println(" ");
+
 		PetController petController = applicationContext.getBean("petController" , PetController.class);
 		System.out.println("-----The Best Pet is-----");
 		System.out.println(petController.whichPetIsTheBest());
 
 		InitializeController initializeController = (InitializeController) applicationContext.getBean("initializeController");
 		System.out.println(initializeController.hello());
+
+		System.out.println(" ");
 
 		System.out.println("-------Primary Bean--------");
 		System.out.println(myController.hello());
@@ -47,6 +52,8 @@ public class GemDiApplication {
 		ConstructorInjectedController constructorInjectedController =(ConstructorInjectedController) applicationContext.getBean("constructorInjectedController");
 		System.out.println(constructorInjectedController.getGreeting());
 
+		System.out.println(" ");
+
 		System.out.println("-------BEAN SCOPES--------");
 
 		SingletonBean singletonBean = applicationContext.getBean(SingletonBean.class);
@@ -54,11 +61,26 @@ public class GemDiApplication {
 		SingletonBean singletonBean2 = applicationContext.getBean(SingletonBean.class);
 		System.out.println(singletonBean2.getMyScope());
 
+		System.out.println(" ");
+
 		PrototypeBean prototypeBean = applicationContext.getBean(PrototypeBean.class);
 		System.out.println(prototypeBean.getMyScope());
 
 		PrototypeBean prototypeBean2 = applicationContext.getBean(PrototypeBean.class);
 		System.out.println(prototypeBean2.getMyScope());
+
+		System.out.println("-------END BEAN SCOPES--------");
+
+		System.out.println(" ");
+
+		System.out.println("------FakeDataSource-----");
+		FakeDataSource fakeDataSource = applicationContext.getBean(FakeDataSource.class);
+		System.out.println(fakeDataSource.getUsername());
+		System.out.println(fakeDataSource.getPassword());
+		System.out.println(fakeDataSource.getJdbcurl());
+		System.out.println("-------End--------");
+
+		System.out.println(" ");
 
 	}
 
--- src/main/java/gem/springboot/gemdi/configuration/GreetingServiceConfig.java
+++ src/main/java/gem/springboot/gemdi/configuration/GreetingServiceConfig.java
@@ -2,19 +2,35 @@ package gem.springboot.gemdi.configuration;
 
 import com.springbootpets.PetService;
 import com.springbootpets.PetServiceFactory;
+import gem.springboot.gemdi.datasource.FakeDataSource;
 import gem.springboot.gemdi.repositories.EnglishGreetingRepository;
 import gem.springboot.gemdi.repositories.EnglishGreetingRepositoryImpl;
 import gem.springboot.gemdi.repositories.SpanishGreetingRepository;
 import gem.springboot.gemdi.repositories.SpanishGreetingRepositoryImpl;
 import gem.springboot.gemdi.services.*;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.*;
 import org.springframework.stereotype.Service;
 
 //@Service
+@PropertySource("classpath:datasource.properties")
 @ImportResource("classpath:gem-config.xml")
 @Configuration
 public class GreetingServiceConfig
 {
+
+    @Bean
+    FakeDataSource fakeDataSource(@Value("${gem.username}") String username ,
+                                  @Value("${gem.password}")  String password ,
+                                  @Value("${gem.jdbcurl}") String jdbcurl)
+    {
+        FakeDataSource fakeDataSource = new FakeDataSource();
+        fakeDataSource.setUsername(username);
+        fakeDataSource.setPassword(password);
+        fakeDataSource.setJdbcurl(jdbcurl);
+
+        return  fakeDataSource;
+    }
 
     @Bean
     PetServiceFactory petServiceFactory()
